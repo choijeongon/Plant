2e4ea69b9521c0d6b8e95a093682e1373e8b19c7
choijeongon <79705809+choijeongon@users.noreply.github.com>
2021-02-26 23:31:38 +0900

    Update constants.dart

--- plant_app/lib/constants.dart
+++ plant_app/lib/constants.dart
@@ -7,5 +7,5 @@ const kBackgroundColor = Color(0xFFF9F8FD);
 const double kDefaultPadding = 20.0;
 
 class ServerUrl {
- static const String url = "http://3.35.120.82:3000/";
+ static const String url = "http://IP:HOST/";
 }
